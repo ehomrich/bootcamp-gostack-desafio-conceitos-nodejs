f6f7b764f83a7b0d176cca2dc4cbcdd4ec5d50d6
Émerson P. Homrich <12125514+ehomrich@users.noreply.github.com>
2020-08-28 22:07:53 -0300

    Remove validator middleware and change error statuses to 400

--- src/app.js
+++ src/app.js
@@ -8,16 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const validateProjectId = (request, response, next) => {
-  const { id } = request.params;
-
-  if (!isUuid(id)) {
-      return response.status(400).json({ error: "Invalid UUID." });
-  }
-
-  return next();
-};
-
 const repositories = [];
 
 app.get('/repositories', (request, response) => response.json(repositories));
@@ -36,13 +26,13 @@ app.post('/repositories', (request, response) => {
   return response.json(repository);
 });
 
-app.put('/repositories/:id', validateProjectId, (request, response) => {
+app.put('/repositories/:id', (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
   const index = repositories.findIndex(repository => repository.id === id);
 
   if (index < 0) {
-    return response.status(404).json({ error: "Repository not found." });
+    return response.status(400).json({ error: "Repository not found." });
   }
 
   const repository = { ...repositories[index], title, url, techs };
@@ -51,12 +41,12 @@ app.put('/repositories/:id', validateProjectId, (request, response) => {
   return response.json(repository);
 });
 
-app.delete('/repositories/:id', validateProjectId, (request, response) => {
+app.delete('/repositories/:id', (request, response) => {
   const { id } = request.params;
   const index = repositories.findIndex(repository => repository.id === id);
 
   if (index < 0) {
-    return response.status(404).json({ error: "Repository not found." });
+    return response.status(400).json({ error: "Repository not found." });
   }
 
   repositories.splice(index, 1);
@@ -64,12 +54,12 @@ app.delete('/repositories/:id', validateProjectId, (request, response) => {
   return response.status(204).send();
 });
 
-app.post('/repositories/:id/like', validateProjectId, (request, response) => {
+app.post('/repositories/:id/like', (request, response) => {
   const { id } = request.params;
   const repository = repositories.find(repository => repository.id === id);
 
   if (!repository) {
-    return response.status(404).json({ error: "Repository not found." });
+    return response.status(400).json({ error: "Repository not found." });
   }
 
   repository.likes += 1;
